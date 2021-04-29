9f0ebada3b8b4fa8fce286729c100637acb6c78b
Evgenij <pe1999@mail.ru>
2021-04-29 23:23:25 +0500

    before lesson-08-hw

---
+++ lesson-08-hw/src/main/java/ru/geekbrains/Lesson08HwApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson08HwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson08HwApplication.class, args);
+    }
+
+}
